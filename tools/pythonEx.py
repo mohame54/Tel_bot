e44bdcc8db86fa28367142cab0cccb28cbb1777e
Mohammed <maymn0535@gmail.com.com>
2024-08-25 13:33:09 +0300

    refactored importutils

--- tools/pythonEx.py
+++ tools/pythonEx.py
@@ -1,6 +1,8 @@
 from .base import BaseTool
 from typing import List, Optional, Union
 import io
+import os
+import subprocess
 import sys
 import threading
 from . import (
@@ -45,7 +47,7 @@ class PythonEx(BaseTool):
         super(PythonEx, self).__init__(tool_name, description)
 
 
-    def _run(self, code_str:str) -> str:
+    def _run_thread_code(self, code_str:str) -> str:
         outputs = []
         errors = []
         # Create a thread to run the code string
@@ -61,15 +63,24 @@ class PythonEx(BaseTool):
         if captured_output:
             return f"\nOutput: \n{captured_output}"
         captured_errors = errors[0] if errors else "" 
-        return f"\n Error: \n{captured_errors}"
+        return f"\nError: \n{captured_errors}"
+    
+    def _save_run_code(self, code_str: str) -> str:
+        with open("tmp_code.py", "w") as file:
+            file.write(code_str)  
+        try:
+            result = subprocess.run(["python", "tmp_code.py"], capture_output=True, text=True, check=True)
+            return f"\nOutput:\n{result.stdout}"
+        except subprocess.CalledProcessError as e:
+            return f"Errors:\n{e.stderr}\nOutput:\n{e.stdout}"
+        finally:
+            os.remove("tmp_code.py")
     
     def _parse_code(self, code: Union[str, List[str]]) -> str:
-
         if isinstance(code, list):
             code = "\n".join(code)
 
         code = code.strip()
-        
         if code.startswith("```") and code.endswith("```"):
             lines = code.splitlines()
             if lines[0].strip().startswith("```") and lines[-1].strip().startswith("```"):
@@ -79,10 +90,12 @@ class PythonEx(BaseTool):
 
         if ";" in code:
             code = code.replace(";", "\n")
-        
         return str(code)
     
     @add_docstring(PYTHON_EX_DOC)
     def __call__(self, code_str: str) -> str:
         code_str = self._parse_code(code_str)
-        return self._run(code_str)
+        # if we are in a machine with more than 1 cpu core use multithreading
+        if os.cpu_count() > 1:
+           return self._run_thread_code(code_str)
+        return self._save_run_code(code_str)
